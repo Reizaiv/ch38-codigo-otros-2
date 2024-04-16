815698b2c79b83b8734fd7c49f0a1c1921715c7b
Javier Villagomez <javier_diaz16@hotmail.com>
2024-04-15 18:17:38 -0600

    Se agregan primeros comentarios al ejercicio 2 de codigo de otros

--- formulario.js
+++ formulario.js
@@ -1,15 +1,15 @@
-var formulario = document.querySelector("#form")
+var formulario = document.querySelector("#formulario") //1 Se cambio el identificador a formulacio
 
 formulario.onsubmit = function(e) {
 
-  e.prevent();
-  
+  e.preventDefault(); //2 Se completo el nombre de la funcion
+
   var n = formulario.elements[0]
   var e = formulario.elements[1]
   var na = formulario.elements[2]
 
   var nombre = n.value
-  var edad = e.value
+  var edad = parseInt(e.value,10) //3 se hizo la conversion del string a un int
 
   var i = na.selectedIndex
   var nacionalidad = na.options[i].value
@@ -19,13 +19,13 @@ formulario.onsubmit = function(e) {
   if (nombre.length === 0) {
     n.classList.add("error")
   }
-  if (edad < 18 || edad > 120) {
+  if (edad < 18 || edad > 80) {  // 4 Se bajo el rango de edad de 120 a 80
     e.classList.add("error")
   }
 
 if (nombre.length > 0 
   && (edad > 18 
-    && edad < 120) ) {
+    && edad <= 80) ) { //5 Se bajo el rango de edad de 120 a 80
   agregarInvitado(nombre, edad, nacionalidad)
   }
 }
@@ -55,7 +55,7 @@ function agregarInvitado(nombre, edad, nacionalidad) {
 var lista = document.getElementById("lista-de-invitados")
 
 var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+elementoLista.classList.add("elemento-lista") //3
 lista.appendChild(elementoLista)
 
 var spanNombre = document.createElement("span")
